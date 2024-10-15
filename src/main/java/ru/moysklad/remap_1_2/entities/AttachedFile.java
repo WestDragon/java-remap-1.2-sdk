ef1dfa7e8aa49b90d590500be3ae8e182a3d6b1f
Ilya Shadryn <i.shadryn@novacom.by>
2024-10-15 23:09:51 +0300

    remove unused import

--- src/main/java/ru/moysklad/remap_1_2/entities/AttachedFile.java
+++ src/main/java/ru/moysklad/remap_1_2/entities/AttachedFile.java
@@ -4,13 +4,8 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.io.FileUtils;
 import ru.moysklad.remap_1_2.entities.agents.Employee;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 
 @Getter
